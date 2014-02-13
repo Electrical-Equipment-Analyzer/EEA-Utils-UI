e2e00aa3a7414f1feba2a84ee45935f2d58eae0d
薛聿明 <102m05008@stud.sju.edu.tw>
2014-02-13 21:50:52 +0800

    update FRA Config.

--- src/main/java/tw/edu/sju/ee/eea/ui/swing/pane/FrequencyResponseConfigPane.java
+++ src/main/java/tw/edu/sju/ee/eea/ui/swing/pane/FrequencyResponseConfigPane.java
@@ -19,31 +19,31 @@ import javax.swing.JOptionPane;
 public class FrequencyResponseConfigPane extends SOptionPanel<FrequencyResponseConfig> {
 
     public static final String NAME = "Frequency Response";
-    private SLabelInput generateChannel = new SLabelInput("Generate Channel");
-    private SLabelInput responseChannel = new SLabelInput("Response Channel");
+    private SLabelInput generateDevice = new SLabelInput("Generate Device");
+    private SLabelInput responseDevice = new SLabelInput("Response Device");
     private SLabelInput voltage = new SLabelInput("Voltage");
     private SLabelInput minFrequency = new SLabelInput("Min Frequency");
     private SLabelInput maxFrequrncy = new SLabelInput("Max Frequency");
     private SLabelInput length = new SLabelInput("Length");
+    private SLabelInput rateMultiple = new SLabelInput("Rate Multiple");
 
     public FrequencyResponseConfigPane() {
         this.setBorder(BorderFactory.createTitledBorder("Configure"));
-        setValue("PXI1Slot3", "Dev1/ai0:1", "2", "200", "200000", "100");
+        setValue("PXI1Slot3", "PXI1Slot4", "2", "30", "3000000", "200", "64");
         initComponents();
     }
 
-    public FrequencyResponseConfigPane(FrequencyResponseConfig config) {
-        this.setBorder(BorderFactory.createTitledBorder("Configure"));
-        setValue(config.getGenerateChannel(), config.getResponseChannel(), String.valueOf(config.getVoltage()),
-                String.valueOf(config.getMinFrequency()), String.valueOf(config.getMaxFrequrncy()), String.valueOf(config.getLength()));
-        initComponents();
-    }
-
+//    public FrequencyResponseConfigPane(FrequencyResponseConfig config) {
+//        this.setBorder(BorderFactory.createTitledBorder("Configure"));
+//        setValue(config.getGenerateChannel(), config.getResponseChannel(), String.valueOf(config.getVoltage()),
+//                String.valueOf(config.getMinFrequency()), String.valueOf(config.getMaxFrequrncy()), String.valueOf(config.getLength()));
+//        initComponents();
+//    }
     public FrequencyResponseConfig getConfig() {
         return new FrequencyResponseConfig(
-                generateChannel.getText(), responseChannel.getText(), Double.parseDouble(voltage.getText()),
+                generateDevice.getText(), responseDevice.getText(), Double.parseDouble(voltage.getText()),
                 Double.parseDouble(minFrequency.getText()), Double.parseDouble(maxFrequrncy.getText()),
-                Integer.parseInt(length.getText()));
+                Integer.parseInt(length.getText()), Integer.parseInt(rateMultiple.getText()));
     }
 
     @Override
@@ -51,28 +51,24 @@ public class FrequencyResponseConfigPane extends SOptionPanel<FrequencyResponseC
         return "Finish";
     }
 
-    private void setValue(String generateChannel, String responseChannel, String voltage, String minFrequency, String maxFrequrncy, String length) {
-        this.generateChannel.setText(generateChannel);
-        this.responseChannel.setText(responseChannel);
+    private void setValue(String generateChannel, String responseChannel, String voltage, String minFrequency, String maxFrequrncy, String length, String rateMultiple) {
+        this.generateDevice.setText(generateChannel);
+        this.responseDevice.setText(responseChannel);
+        this.voltage.setText(voltage);
         this.minFrequency.setText(minFrequency);
         this.maxFrequrncy.setText(maxFrequrncy);
-        this.voltage.setText(voltage);
         this.length.setText(length);
+        this.rateMultiple.setText(rateMultiple);
     }
 
     private void initComponents() {
-        this.add(generateChannel);
-        this.add(responseChannel);
+        this.add(generateDevice);
+        this.add(responseDevice);
+        this.add(voltage);
         this.add(minFrequency);
         this.add(maxFrequrncy);
-        this.add(voltage);
         this.add(length);
-//        generateChannel.setLocation(50, 50);
-//        responseChannel.setLocation(50, 100);
-//        minFrequency.setLocation(50, 150);
-//        maxFrequrncy.setLocation(50, 200);
-//        voltage.setLocation(50, 300);
-//        length.setLocation(50, 300);
+        this.add(rateMultiple);
 
         GroupLayout layout = new GroupLayout(this);
         this.setLayout(layout);
@@ -80,23 +76,25 @@ public class FrequencyResponseConfigPane extends SOptionPanel<FrequencyResponseC
                 layout.createSequentialGroup()
                 .addGap(10)
                 .addGroup(layout.createParallelGroup()
-                        .addComponent(generateChannel)
-                        .addComponent(responseChannel)
+                        .addComponent(generateDevice)
+                        .addComponent(responseDevice)
+                        .addComponent(voltage)
                         .addComponent(minFrequency)
                         .addComponent(maxFrequrncy)
-                        .addComponent(voltage)
-                        .addComponent(length))
+                        .addComponent(length)
+                        .addComponent(rateMultiple))
                 .addGap(10)
         );
         layout.setVerticalGroup(
                 layout.createSequentialGroup()
                 .addGap(20)
-                .addComponent(generateChannel, 30, 30, 30)
-                .addComponent(responseChannel, 30, 30, 30)
+                .addComponent(generateDevice, 30, 30, 30)
+                .addComponent(responseDevice, 30, 30, 30)
+                .addComponent(voltage, 30, 30, 30)
                 .addComponent(minFrequency, 30, 30, 30)
                 .addComponent(maxFrequrncy, 30, 30, 30)
-                .addComponent(voltage, 30, 30, 30)
                 .addComponent(length, 30, 30, 30)
+                .addComponent(rateMultiple, 30, 30, 30)
                 .addGap(20)
         );
 
